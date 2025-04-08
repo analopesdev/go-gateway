da459ef8c8087345c966ce131e1ade0b5cc08c8e
analopesdev <analopesdemorais@outlook.com>
2025-04-08 20:37:18 -0300

    Adiciona suporte a variáveis de ambiente e configuração de conexão com o banco de dados no main.go; inclui dependências do pacote github.com/go-chi/chi/v5 e github.com/joho/godotenv no go.mod

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/analopesdev/go-gateway/internal/repository"
+	"github.com/analopesdev/go-gateway/internal/service"
+	"github.com/analopesdev/go-gateway/internal/web/server"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	port := os.Getenv("HTTP_PORT")
+
+	srv := server.NewServer(accountService, port)
+	srv.ConfigureRoutes()
+
+	if err := srv.Start(); err != nil {
+		log.Fatal("Error starting server")
+	}
 }
